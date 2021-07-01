de13452e719df24d9907f6aef64e66a1ebcdfc52
rahult-rt <71532679+rahult-rt@users.noreply.github.com>
2021-07-01 00:15:24 -0700

    Add files via upload

---
+++ array/arrayMethods3.js
@@ -0,0 +1,29 @@
+arr = [1, 2, 3, 5, 8];
+// result = 0;
+// arr.forEach((ele) => {
+//   result += ele;
+// });
+
+//string,number,array,object
+
+// const check = arr.reduce((acc, currentValue) => {
+//   console.log("== ACC ==", acc);
+//   console.log("== CURRENT ==", currentValue);
+//   acc += currentValue;
+//   console.log("=== AFTERR ADD ==", acc);
+//   return acc;
+// }, 0);
+
+// console.log(check);
+
+const str = "hello"; //['h','e']
+
+const newStr = str.split("");
+
+//['h1','w1']
+const result = newStr.reduce((acc, currentValue) => {
+  console.log(acc);
+  acc.push((currentValue += 1));
+  return acc;
+}, []);
+console.log(result);
