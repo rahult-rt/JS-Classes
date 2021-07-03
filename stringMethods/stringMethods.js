0264594e1a56ed2d5b818e965920e3c0eb577c4f
rahult-rt <71532679+rahult-rt@users.noreply.github.com>
2021-07-03 13:49:11 -0700

    Add files via upload

---
+++ stringMethods/stringMethods.js
@@ -0,0 +1,37 @@
+//includes()
+//returns boolean if
+const str = "This is Test string";
+//console.log(str.includes("Is"));
+
+//charAt()
+// returns the character at soecified index
+//console.log("== CHAR AT ", typeof str.charAt(-1));
+
+//startsWith()
+//console.log(str.startsWith("this", 0));
+// const email = "something.com";
+// console.log(email.endsWith(".com"));
+// console.log(str.length);
+
+//indexOf()
+// const email = "something@com";
+// console.log(email.indexOf("@"));
+
+// console.log(str.concat("hi"));
+
+//replace()
+
+//console.log(str.replace(" ", "/"));
+
+//substr()
+// console.log(str.substr(0, -2));
+
+// console.log(str.slice(-3, -1));
+
+// const inputVal = "call";
+
+// console.log(inputVal.split(""));
+
+console.log(str.split(" ").join("/"));
+
+//console.log(str.join(""));
