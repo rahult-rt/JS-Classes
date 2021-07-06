20f5f111f9e0819301c6103353503d895e4777da
rahult-rt <71532679+rahult-rt@users.noreply.github.com>
2021-07-06 14:07:02 -0700

    Update assignment6.js

--- Assignments/assignment6.js
+++ Assignments/assignment6.js
@@ -1,6 +1,28 @@
 // Your task is to arrange all the even elements at the start followed by the odd numbers
-let arr1 = [1, 6, 9, 3, 10, 5, 12, 2];
+
+const arr1 = [1, 6, 9, 3, 10, 5, 12, 2];
+
+let result =[]
+
+for (let i=0;i<arr1.length;i++){
+    
+    if(arr1[i]%2!==0) result.push(arr1[i])
+    if(arr1[i]%2===0) result.unshift(arr1[i])
+
+}
+
+console.log(result);
+
 
 // Move All the zeros in the given array at the end of the array;
 
 const moveZeros = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
+
+let result =[]
+
+for (let i=0;i<moveZeros.length;i++){
+    if(moveZeros[i]===0) result.push(moveZeros[i])
+    if(moveZeros[i]!==0) result.unshift(moveZeros[i])
+}
+
+console.log(result);
