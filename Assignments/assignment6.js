a49efd7eff4c22030680504adf0d56cdb7e2e1d1
rahult-rt <71532679+rahult-rt@users.noreply.github.com>
2021-07-05 20:48:14 -0700

    Add files via upload

---
+++ Assignments/assignment6.js
@@ -0,0 +1,6 @@
+// Your task is to arrange all the even elements at the start followed by the odd numbers
+let arr1 = [1, 6, 9, 3, 10, 5, 12, 2];
+
+// Move All the zeros in the given array at the end of the array;
+
+const moveZeros = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
