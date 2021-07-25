59367dae814a39464028caa64eb908159af3df72
rahult-rt <71532679+rahult-rt@users.noreply.github.com>
2021-07-25 08:36:18 -0700

    Add files via upload

---
+++ asyncJs/index.js
@@ -0,0 +1,28 @@
+// console.log(num);
+// // console.log(test);
+
+// var num = 10;
+// const test = "helow"
+
+// let right = true
+// function check(n){
+//     var m = n*2
+//     const value = m
+//     return value
+// }
+
+// var square1 = check(4);
+
+// console.log(square1);
+
+// var square2 = check(num);
+
+// console.log(square2);
+
+console.log("Test 1");
+console.log("Test 2");
+
+setTimeout(()=>{
+console.log("Inside Time");
+},4000)
+console.log("Test 3");
